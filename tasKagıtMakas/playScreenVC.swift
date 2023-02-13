f187f5149b77b6606aab5232cfa65430615e8d7d
ragibgormez <ragib.gormez@gmail.com>
2023-02-14 00:29:56 +0300

    sade hali

--- tasKagıtMakas/playScreenVC.swift
+++ tasKagıtMakas/playScreenVC.swift
@@ -39,35 +39,7 @@ class playScreenVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        scissorsImageView1.isUserInteractionEnabled = true
-        scissorsImageView2.isUserInteractionEnabled = true
-        scissorsImageView3.isUserInteractionEnabled = true
-        stoneImageView1.isUserInteractionEnabled = true
-        stoneImageView2.isUserInteractionEnabled = true
-        stoneImageView3.isUserInteractionEnabled = true
         
-        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-
-
-        scissorsImageView1.addGestureRecognizer(recognizer1)
-        scissorsImageView2.addGestureRecognizer(recognizer2)
-        scissorsImageView3.addGestureRecognizer(recognizer3)
-        stoneImageView1.addGestureRecognizer(recognizer4)
-        stoneImageView2.addGestureRecognizer(recognizer5)
-        stoneImageView3.addGestureRecognizer(recognizer6)
-        paperImageView1.addGestureRecognizer(recognizer7)
-        paperImageView2.addGestureRecognizer(recognizer8)
-        paperImageView3.addGestureRecognizer(recognizer9)
-        */
     }
     
     @objc func increaseScore() {
